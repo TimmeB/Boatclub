8579b40bcc6d2cafbe0d3312125f3743d4e8d79c
Er1kmedk <38457788+Er1kmedk@users.noreply.github.com>
2018-09-26 11:42:12 +0200

    writeToMemberList
    first quick merge of writeToMemberList

--- src/model/Registry.java
+++ src/model/Registry.java
@@ -2,8 +2,15 @@ package model;
 
 import java.util.ArrayList;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Registry {
+	
 	private ArrayList<Member> memberList;
+	private ObjectMapper objectMapper = new ObjectMapper();
 	
 	public Registry() {
 		memberList = new ArrayList<>();
@@ -15,7 +22,17 @@ public class Registry {
 		System.out.println("hej");
 	}
 	
-	public String toString() {
+	public String toString() {		// denna metod kommer ersätttas av hämtmedtod från fil
 		return memberList.get(0).getMemberName() + " " + memberList.get(0).getMemberpNum();
+	}
+	 
+	
+	public void writeToMemberList(ArrayList listToBeWrote) throws JsonMappingException {
+		//objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+		objectMapper.updateValue("C:\\Users\\erikm\\Desktop\\Uppgifter\\OOAD UML 1DV607\\Boatclub", memberList);
+	}
+	
+	public void readFromMemberList() {
+		
 	}
 }
